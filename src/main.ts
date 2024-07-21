a7341a72da09b3a9cf973962c1f7660852536346
Kok4ik <153661513+Kok4ik@users.noreply.github.com>
2024-07-21 17:18:12 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const start = async () => {
         const app = await NestFactory.create(AppModule) 
         const configService = app.get(ConfigService)
         const PORT = configService.get<number>('PORT')
-        app.enableCors({origin: 'https://music-client-delta.vercel.app/'})
+        app.enableCors({origin: 'https://music-client-delta.vercel.app'})
         await app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
     } catch (error) {
         console.log(error)
